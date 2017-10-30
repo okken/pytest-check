2e8bfafed8c77b725830b49dff49bca58c2e2c7f
Brian Okken <brian@pythontesting.net>
2017-10-30 08:07:44 -0700

    Check for the failure strings during failed tests

--- tests/test_check.py
+++ tests/test_check.py
@@ -18,6 +18,15 @@ def test_check_equal_fail(testdir):
             check.equal(1, 2)
             check.equal((1, 2, 3), (1, 2, 4))
     """)
+
     result = testdir.runpytest('-v', '-s')
 
     result.assert_outcomes(failed=1)
+
+    result.stdout.fnmatch_lines([
+        'FAILURE:*',
+        '*line 2, in test_fail() -> check.equal(1, 2)*',
+        'FAILURE:*',
+        '*line 3, in test_fail() -> check.equal((1, 2, 3), (1, 2, 4))',
+        'Failed Checks: 2*'
+    ])
