08ccb7c7a374329d90dc81c876fef37e9aeafe6a
Brian Okken <1568356+okken@users.noreply.github.com>
2022-11-22 10:04:50 -0800

    formatting

--- examples/test_example_check_check.py
+++ examples/test_example_check_check.py
@@ -5,12 +5,13 @@
 However, we want both to work.
 """
 
+
 def test_check(check):
     with check:
         assert True
+
 
 def test_check_check(check):
     "Deprecated, but should still work for now"
     with check.check:
         assert True
-
