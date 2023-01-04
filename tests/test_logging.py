d5854c294ee0d8d683e194508ca9319ba0be477a
Angel Luis Blasco <22562761+alblasco@users.noreply.github.com>
2023-01-04 23:51:55 +0100

    unit test

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,21 +1,21 @@
 
-# def test_logging_level_error(pytester):
-#     """
-#     Should log 2 failures inside log with error level
-#     """
-#     pytester.copy_example("examples/test_example_logging.py")
-#     result = pytester.runpytest(
-#         "--logging-level=ERROR -k test_logging_error_level"
-#     )
-#     result.assert_outcomes(failed=1, passed=0)
-#     result.stdout.fnmatch_lines(["* 2 failed *"])
-#
-#
-# def test_logging_no_level(pytester):
-#     """
-#     Should not log failures
-#     """
-#     pytester.copy_example("examples/test_example_logging.py")
-#     result = pytester.runpytest("-k test_logging_no_level")
-#     result.assert_outcomes(failed=1, passed=0)
-#     result.stdout.fnmatch_lines(["* 2 failed *"])
+def test_logging_level_error(pytester):
+    """
+    Should log 2 failures inside log with error level
+    """
+    pytester.copy_example("examples/test_example_logging.py")
+    result = pytester.runpytest(
+        "--logging-level=ERROR -k test_logging_error_level"
+    )
+    result.assert_outcomes(failed=1, passed=0)
+    result.stdout.fnmatch_lines(["* 2 failed *"])
+
+
+def test_logging_no_level(pytester):
+    """
+    Should not log failures
+    """
+    pytester.copy_example("examples/test_example_logging.py")
+    result = pytester.runpytest("-k test_logging_no_level")
+    result.assert_outcomes(failed=1, passed=0)
+    result.stdout.fnmatch_lines(["* 2 failed *"])
