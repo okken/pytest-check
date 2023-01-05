1a55b2051474b24b8da8c45c1cf67fffd79d23f9
Brian Okken <1568356+okken@users.noreply.github.com>
2023-01-05 14:40:22 -0800

    -> --check-log-level

---
+++ examples/test_example_logging.py
@@ -0,0 +1,45 @@
+"""
+A test file to try with without check-log-level option
+"""
+from pytest_check import check
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def test_error_level(caplog):
+    log.warning("First check")
+    check.equal("one", "xxx")
+    log.warning("Second check")
+    check.equal("two", "yyy")
+
+    assert len(caplog.record_tuples) == 4
+
+    message = caplog.record_tuples[0][2]
+    assert message == 'First check'
+
+    _, level, message = caplog.record_tuples[1]
+    assert level == logging.ERROR
+    assert 'FAILURE: check one' in message
+
+    message = caplog.record_tuples[2][2]
+    assert message == 'Second check'
+
+    _, level, message = caplog.record_tuples[3]
+    assert level == logging.ERROR
+    assert 'FAILURE: check two' in message
+
+
+def test_no_level(caplog):
+    log.warning("First check")
+    check.equal("one", "xxx")
+    log.warning("Second check")
+    check.equal("two", "yyy")
+
+    assert len(caplog.record_tuples) == 2
+
+    message = caplog.record_tuples[0][2]
+    assert message == 'First check'
+
+    message = caplog.record_tuples[1][2]
+    assert message == 'Second check'
---
+++ tests/test_logging.py
@@ -0,0 +1,21 @@
+def test_logging_error_level(pytester):
+    """
+    Should log 2 failures inside log with error level
+    """
+    pytester.copy_example("examples/test_example_logging.py")
+    result = pytester.runpytest(
+        "test_example_logging.py::test_error_level",
+        "--check-log-level=ERROR",
+    )
+    result.assert_outcomes(failed=1, passed=0)
+    result.stdout.fnmatch_lines(["Failed Checks: 2"])
+
+
+def test_logging_no_level(pytester):
+    """
+    Should not log failures
+    """
+    pytester.copy_example("examples/test_example_logging.py")
+    result = pytester.runpytest("test_example_logging.py::test_no_level")
+    result.assert_outcomes(failed=1, passed=0)
+    result.stdout.fnmatch_lines(["Failed Checks: 2"])
