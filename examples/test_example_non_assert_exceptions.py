60d87c4ddbe41f79d5ad17cc8ffff1d7eac2ca32
Brian Okken <1568356+okken@users.noreply.github.com>
2026-03-20 08:29:00 -0700

    fix mypy issue

--- examples/test_example_non_assert_exceptions.py
+++ examples/test_example_non_assert_exceptions.py
@@ -3,19 +3,20 @@ Non-assert exceptions should also be caught by check.
 All of these should report "Failed Checks: 1"
 """
 
+
 def test_index_error(check):
-    a_list = []
+    a_list: list[str] = []
     with check:
-        assert a_list[-1] == "Expected Value"  
+        assert a_list[-1] == "Expected Value"
 
 
 def test_key_error(check):
-    a_dict = {}
+    a_dict: dict[str, str] = {}
     with check:
-        assert a_dict["k"] == "v"  
+        assert a_dict["k"] == "v"
+
 
 def test_attribute_error(check):
-    an_object = object
+    an_object: object = object()
     with check:
-        assert an_object.v == "obj"  
-
+        assert an_object.v == "obj"  # type: ignore[attr-defined]
